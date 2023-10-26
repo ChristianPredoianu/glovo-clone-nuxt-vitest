de9dfe4144b35d3524970d27e3a7baa5fdfb11f8
Christian Predoianu <christianpredoianu@yahoo.com>
2023-10-26 12:04:21 +0200

    added config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-vitest'],
+  googleFonts: {
+    // Options
+    families: {
+      Raleway: {
+        // weights
+        wght: [100, 400],
+        // italic
+        ital: [100],
+      },
+    },
+  },
+  components: [
+    { path: '@/components/header', extensions: ['vue'] },
+    { path: '@/components/ui', extensions: ['vue'] },
+  ],
+});
