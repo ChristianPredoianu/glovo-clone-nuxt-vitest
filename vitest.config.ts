eecbb376de1574eb334d2f078886c91e41c8952b
Christian Predoianu <christianpredoianu@yahoo.com>
2023-10-31 16:44:09 +0100

    config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,15 @@
 import * as path from 'path';
-import vue from '@vitejs/plugin-vue';
 import { defineVitestConfig } from 'nuxt-vitest/config';
 
 const r = (p: string) => path.resolve(__dirname, p);
 
 export default defineVitestConfig({
-  plugins: [vue()],
   test: {
     environment: 'nuxt',
+    globals: true,
   },
+  plugins: [],
+
   resolve: {
     alias: {
       '@': r('.'),
