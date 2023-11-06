3960be4f17e359bd33d9688791bdeaee7b37cfdf
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-06 16:34:59 +0100

    work

--- components/ui/backdrop.nuxt.test.ts
+++ components/ui/backdrop.nuxt.test.ts
@@ -1,21 +1,15 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render } from '@testing-library/vue';
+import { mount } from '@vue/test-utils';
 import { mountSuspended } from 'nuxt-vitest/utils';
-import { screen } from '@testing-library/vue';
 import Backdrop from '@/components/ui/Backdrop.vue';
 
 describe('Backdrop', () => {
   it('should close the backdrop when it is clicked', async () => {
-    const wrapper = await mountSuspended(Backdrop);
+    const wrapper = mount(Backdrop);
 
-    const backdrop = wrapper.find('[data-testid="backdrop"]');
-    console.log(backdrop.exists());
-    /* expect(backdrop.exists()).toBe(false); */
+    const backdrop = wrapper.find('div');
 
-    /*    const backdrop = wrapper.find('[data-testid="backdrop"]');
     expect(backdrop.exists()).toBe(false);
-
-    await wrapper.vm.closeBackdrop();
-    expect(backdrop.exists()).toBe(true); */
   });
 });
