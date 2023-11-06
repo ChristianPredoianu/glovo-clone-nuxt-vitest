5f654ed6b43feba5912063c024e30cfdd7d86957
Christian Predoianu <christianpredoianu@yahoo.com>
2023-11-06 14:28:57 +0100

    work

--- app.vue
+++ app.vue
@@ -2,7 +2,6 @@
 
 <template>
   <div>
-    <Backdrop />
     <AppHeader />
     <NuxtPage />
   </div>
--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -8,9 +8,12 @@ useBackdrop(closeNav);
   <header class="relative bg-yellow-300">
     <nav class="flex items-center justify-between flex-wrap container mx-auto p-4">
       <span class="font-semibold text-green-500 text-xl">GlovoApp</span>
+      <Backdrop />
       <Hamburger />
       <div
-        :class="isNavOpen ? ['translate-x-[0rem]'] : ['-translate-x-[50rem]']"
+        :class="
+          isNavOpen ? ['translate-x-[0rem] opened'] : ['-translate-x-[50rem] closed']
+        "
         class="list-div absolute z-50 top-0 rounded-r-2xl sm:rounded-r-none left-0 sm:relative w-10/12 nav-list-div min-h-screen sm:min-h-full flex-grow bg-white py-4 sm:py-0 sm:flex sm:items-center sm:translate-x-0 sm:ml-4 sm:w-auto transition ease-in-out"
       >
         <NavList />
