71685154e8c7f0720958fee84f871504be0bf286
Christian Predoianu <christianpredoianu@yahoo.com>
2024-01-09 20:55:01 +0100

    finished test

--- components/header/hamburger.nuxt.test.ts
+++ components/header/hamburger.nuxt.test.ts
@@ -1,22 +1,18 @@
-import { it, describe, expect, vi } from 'vitest';
-import { mountSuspended } from 'nuxt-vitest/utils';
-import { shallowMount } from '@vue/test-utils';
+import { it, describe, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
 import Hamburger from '@/components/header/Hamburger.vue';
+import AppHeader from '@/components/header/AppHeader.vue';
 
 describe('Hamburger', () => {
   it('should toggle navigation menu on hamburger button click', async () => {
-    const wrapper = await mountSuspended(Hamburger);
-    /*  const wrapper = shallowMount(Hamburger); */
+    const hamburgerWrapper = mount(Hamburger);
+    const appHeaderWrapper = mount(AppHeader);
 
-    /*  const spy = vi.spyOn(useNav, 'openNav'); */
-    const spy = vi.spyOn(wrapper.vm, 'openNavigation');
-    /* const mock = vi.fn().mockImplementation(openNav); */
-    /* const spy = vi.spyOn(wrapper.vm, 'openNavigation'); */
+    const navListDiv = appHeaderWrapper.find('#test');
 
-    const btn = wrapper.find('button');
+    const btn = hamburgerWrapper.find('button');
     await btn.trigger('click');
-    await wrapper.vm.openNavigation();
 
-    expect(spy).toHaveBeenCalled();
+    expect(navListDiv.classes()).toContain('translate-x-[0rem]');
   });
 });
