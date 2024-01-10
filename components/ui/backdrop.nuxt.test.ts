a860ed7c8cd2e2580c7ad904c9ebdc648971c457
Christian Predoianu <christianpredoianu@yahoo.com>
2024-01-10 14:14:51 +0100

    testing backdrop

--- components/ui/backdrop.nuxt.test.ts
+++ components/ui/backdrop.nuxt.test.ts
@@ -1,13 +1,25 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Backdrop from '@/components/ui/Backdrop.vue';
+import Hamburger from '@/components/header/Hamburger.vue';
 
 describe('Backdrop', () => {
-  it('should be closed', async () => {
-    const wrapper = mount(Backdrop);
+  const backdropWrapper = mount(Backdrop);
+  const hamburgerWrapper = mount(Hamburger);
 
-    const backdrop = wrapper.find('div');
+  it('should be closed', async () => {
+    const backdrop = backdropWrapper.find('div');
 
     expect(backdrop.exists()).toBe(false);
+  });
+
+  it('should be opened', async () => {
+    const hamburger = hamburgerWrapper.find('button');
+
+    await hamburger.trigger('click');
+
+    const backdrop = backdropWrapper.find('div');
+
+    expect(backdrop.classes()).toContain('w-full');
   });
 });
