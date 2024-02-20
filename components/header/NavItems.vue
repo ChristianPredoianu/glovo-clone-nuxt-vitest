f102e69391fead5fb23f03a036de340db624f65f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-20 20:03:58 +0100

    work

--- components/header/NavItems.vue
+++ components/header/NavItems.vue
@@ -5,19 +5,25 @@ const { closeBackdrop } = useBackdrop(closeNav);
 </script>
 
 <template>
-  <div class="flex flex-col h-full gap-8">
+  <div class="flex flex-col h-full gap-8 container px-4 mx-auto">
     <font-awesome-icon
-      class="absolute top-6 right-6"
+      class="absolute top-6 right-6 cursor-pointer"
       :icon="['fas', 'arrow-left']"
       size="xl"
       v-if="screenWidth < 640"
       @click="closeBackdrop"
     />
-    <div class="border-b-2 w-full flex justify-center mt-40" v-if="screenWidth < 640">
-      <h1 class="text-2xl pb-2">
-        <span class="text-green-500">Connect</span> | New Account
+    <div class="border-b-2 flex mt-40" v-if="screenWidth < 640">
+      <h1
+        class="pb-2 text-md"
+        :class="{
+          'text-xs': screenWidth < 200,
+        }"
+      >
+        <span class="text-green-500">Connect</span>
+        | New Account
       </h1>
     </div>
-    <NavLinks />
+    <NavList />
   </div>
 </template>
