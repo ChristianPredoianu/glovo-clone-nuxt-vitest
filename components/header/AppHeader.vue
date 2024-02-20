d3849a9e6bcf66d0f164548ce4b9f458ff70d93f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-20 13:05:05 +0100

    work

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -7,15 +7,16 @@ useBackdrop(closeNav);
 <template>
   <header class="relative bg-yellow-300">
     <nav class="flex items-center justify-between flex-wrap container mx-auto p-4">
-      <span class="font-semibold text-green-500 text-xl">GlovoApp</span>
+      <Logo />
       <Backdrop />
       <Hamburger />
+
       <div
         id="nav-list-div"
         :class="isNavOpen ? ['translate-x-[0rem]'] : ['-translate-x-[50rem]']"
-        class="bg-white sm:bg-transparent absolute sm:relative z-50 top-0 left-0 w-10/12 sm:w-auto min-h-screen sm:min-h-full flex flex-grow flex-col items-center justify-center sm:justify-start sm:flex-row rounded-r-2xl sm:rounded-r-none transition ease-in-out py-4 sm:py-0 sm:translate-x-0 sm:ml-4"
+        class="h-full bg-white sm:bg-transparent absolute sm:relative z-50 top-0 left-0 w-10/12 sm:w-auto min-h-screen sm:min-h-full flex flex-grow flex-col items-center justify-center sm:justify-start sm:flex-row rounded-r-2xl sm:rounded-r-none transition ease-in-out sm:py-0 sm:translate-x-0 sm:ml-4"
       >
-        <NavList />
+        <NavItems />
       </div>
     </nav>
   </header>
