5ebf2fd63698cdfde54be566ca7ee4aa5ea5a330
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-20 13:37:19 +0100

    final

--- components/ui/backdrop.nuxt.test.ts
+++ components/ui/backdrop.nuxt.test.ts
@@ -1,34 +1,22 @@
-import { nextTick } from 'vue';
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Backdrop from '@/components/ui/Backdrop.vue';
-import Hamburger from '@/components/header/Hamburger.vue';
 
 describe('Backdrop', () => {
-  const backdropWrapper = mount(Backdrop);
-  const hamburgerWrapper = mount(Hamburger);
-
   it('should be closed', async () => {
-    const backdrop = backdropWrapper.find('div');
-
-    expect(backdrop.exists()).toBe(false);
+    const { isBackdropOpen } = useBackdrop();
+    expect(isBackdropOpen.value).toBe(false);
   });
 
-  it('should be opened when clicking hamburger', async () => {
-    const hamburger = hamburgerWrapper.find('button');
-
-    await hamburger.trigger('click');
-
-    const backdrop = backdropWrapper.find('div');
-
-    expect(backdrop.classes()).toContain('w-full');
+  it('should be opened when openBackdrop is called', async () => {
+    const { isBackdropOpen, openBackdrop } = useBackdrop();
+    openBackdrop();
+    expect(isBackdropOpen.value).toBe(true);
   });
 
-  it('should be closed when clicking backdrop', async () => {
-    const backdrop = backdropWrapper.find('div');
-
-    await backdrop.trigger('click');
-
-    expect(backdrop.exists()).toBe(false);
+  it('should be closed when closeBackdrop is called', async () => {
+    const { isBackdropOpen, closeBackdrop } = useBackdrop();
+    closeBackdrop();
+    expect(isBackdropOpen.value).toBe(false);
   });
 });
