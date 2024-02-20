64ff8ad203325790613b90e5107187b8e3fd2a43
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-20 13:03:07 +0100

    work

---
+++ components/header/NavItems.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { screenWidth } = useScreenWidth();
+const { closeNav } = useNav();
+const { closeBackdrop } = useBackdrop(closeNav);
+</script>
+
+<template>
+  <div class="flex flex-col h-full gap-8">
+    <font-awesome-icon
+      class="absolute top-6 right-6"
+      :icon="['fas', 'arrow-left']"
+      size="xl"
+      v-if="screenWidth < 640"
+      @click="closeBackdrop"
+    />
+    <div class="border-b-2 w-full flex justify-center mt-40" v-if="screenWidth < 640">
+      <h1 class="text-2xl pb-2">
+        <span class="text-green-500">Connect</span> | New Account
+      </h1>
+    </div>
+    <NavLinks />
+  </div>
+</template>
