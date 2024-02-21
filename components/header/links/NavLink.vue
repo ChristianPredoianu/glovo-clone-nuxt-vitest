e449b8ad6370e0856ef9dd44f8b2f9cfc2afea74
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-21 11:41:07 +0100

    fixed window bug

--- components/header/links/NavLink.vue
+++ components/header/links/NavLink.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
 <template>
   <div class="flex items-center justify-between border-b-2 sm:border-0 cursor-pointer">
     <li
-      class="block text-2xl font-semibold sm:text-white sm:text-sm sm:inline-block text-teal-lighter sm:hover:text-white cursor-pointer sm:mr-4"
+      class="block text-2xl font-semibold text-gray-600 sm:text-sky-900 sm:text-sm sm:inline-block text-teal-lighter sm:hover:text-white cursor-pointer sm:mr-4"
       :class="{
         'text-xs': screenWidth < 230,
       }"
