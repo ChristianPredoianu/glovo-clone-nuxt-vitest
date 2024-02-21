21cead56fddf938fd13df722302a4c69b84e9d5e
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-21 12:33:19 +0100

    added pages folder

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="bg-yellow-400 text-gray-800">
+    <div
+      class="container px-4 pt-28 mx-auto flex flex-col items-center justify-center gap-8 p-10 md:flex-row"
+    >
+      <div class="w-1/2 md:w-1/3">
+        <img src="@/assets/food.png" alt="food" class="md:w-1/2" />
+      </div>
+      <div class="bg-yellow-400 text-center">
+        <h1 class="text-2xl font-bold md:text-4xl">Food delivery and more</h1>
+        <p class="mt-2 font-medium md:font-xl">Groceries, shops, pharmacies, anything!</p>
+      </div>
+    </div>
+  </section>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path
+      fill="#facc15"
+      fill-opacity="1"
+      d="M0,32L60,48C120,64,240,96,360,106.7C480,117,600,107,720,122.7C840,139,960,181,1080,170.7C1200,160,1320,96,1380,64L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+    ></path>
+  </svg>
+</template>
