cc1ea54aca959f79a37a8492c90bdca934272a26
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-21 11:41:45 +0100

    added components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,7 @@ export default defineNuxtConfig({
   css: ['@fortawesome/fontawesome-svg-core/styles.css'],
   components: [
     { path: '@/components/header', extensions: ['vue'] },
+    { path: '@/components/header/links', extensions: ['vue'] },
     { path: '@/components/ui', extensions: ['vue'] },
   ],
 });
