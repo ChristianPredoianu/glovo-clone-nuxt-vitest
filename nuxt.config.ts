17c0bcaa6a42329521d03187fcae118f91df99b0
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-23 14:16:39 +0100

    work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
-    apiSecret: process.env.NUXT_PUBLIC_API_BASE,
+    apiSecret: process.env.NUXT_API_SECRET,
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
     },
