9229b174f7e6e80b61b63029a432cff57f81876d
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-26 13:42:31 +0100

    made the component reusable

--- components/ui/Dropdown.vue
+++ components/ui/Dropdown.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
-import type { ILocationsData, ILocationsOption } from '@/interfaces/locations.interface';
+interface IOptions {
+  place_id: number;
+  display_name: string;
+}
 
 const props = defineProps<{
-  locationsData: ILocationsData[] | null;
+  options: { place_id: number; display_name: string }[];
 }>();
+
+console.log(props.options);
 
 const isOpen = useState<boolean>('isOpen', () => true);
 const selectedOption = useState<string>('selectedOption', () => 'Select an option');
+
+const emits = defineEmits(['emit-option']);
 
 const toggleDropdown = () => {
   isOpen.value = !isOpen.value;
 };
 
-const selectOption = (option: ILocationsOption) => {
+const selectOption = (option: IOptions) => {
   selectedOption.value = option.display_name;
   isOpen.value = false;
 };
@@ -26,7 +33,7 @@ const selectOption = (option: ILocationsOption) => {
     >
       <ul>
         <li
-          v-for="option in props.locationsData"
+          v-for="option in props.options"
           :key="option.place_id"
           class="py-2 px-4 hover:bg-gray-100 cursor-pointer"
           @click="selectOption(option)"
