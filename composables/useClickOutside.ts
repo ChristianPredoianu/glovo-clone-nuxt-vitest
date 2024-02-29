826662b32900be33d29f3f37c4a729c8057d0a55
Christian Predoianu <christianpredoianu@yahoo.com>
2024-02-29 15:27:04 +0100

    added composable

---
+++ composables/useClickOutside.ts
@@ -0,0 +1,18 @@
+export function useClickOutside(
+  elementRef: Ref<HTMLElement | null>,
+  callback: () => void
+) {
+  const handleClick = (event: MouseEvent) => {
+    if (elementRef.value && !elementRef.value.contains(event.target as Node)) {
+      callback();
+    }
+  };
+
+  onMounted(() => {
+    document.addEventListener('click', handleClick);
+  });
+
+  onUnmounted(() => {
+    document.removeEventListener('click', handleClick);
+  });
+}
