169c3216124e7699a3d9d9aecb8ff4b373b3cf23
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-04 13:56:54 +0100

    added comp

---
+++ components/cards/CategoryCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  img: string;
+  text: string;
+}>();
+</script>
+
+<template>
+  <article
+    class="flex flex-col items-center transform transition-transform duration-300 hover:scale-105 cursor-pointer"
+  >
+    <img :src="props.img" :alt="props.text" class="w-20 h-20 rounded-full object-cover" />
+
+    <h3 class="mt-3 text-center text-sm">{{ props.text }}</h3>
+  </article>
+</template>
