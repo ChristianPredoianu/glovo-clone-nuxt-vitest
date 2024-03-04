fcc43f5f4247d2ccacff35a0fa9f29b057973342
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-04 15:56:42 +0100

    extracted waves to own component

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,9 @@ const emittedOption = useState<IDropdownOptions>('emittedOption', () => {
 });
 
 const emittedLocation = useState<string>('emittedLocation', () => '');
+let dropdownOptions: IDropdownOptions[] = [];
 
 const runtimeConfig = useRuntimeConfig();
-
-let dropdownOptions: IDropdownOptions[] = [];
 
 const { data } = await useFetch<ILocationsData[]>(
   () =>
@@ -64,17 +63,15 @@ watch(
           Groceries, clothing, electronics, anything!
         </p>
         <div class="input-container relative mt-8">
-          <Suspense>
-            <AdressForm
-              v-if="data || dropdownOptions"
-              :options="dropdownOptions"
-              textKey="text"
-              idKey="id"
-              @inputRefEmit="handleEmittedSearchQuery"
-              @emit-option="handleEmmitedOption"
-              @emit-location="handleEmmitedLocation"
-            />
-          </Suspense>
+          <AdressForm
+            v-if="data || dropdownOptions"
+            :options="dropdownOptions"
+            textKey="text"
+            idKey="id"
+            @inputRefEmit="handleEmittedSearchQuery"
+            @emit-option="handleEmmitedOption"
+            @emit-location="handleEmmitedLocation"
+          />
 
           <p
             class="text-sm md:text-lg font-medium mt-4 absolute top-12 left-0"
@@ -103,12 +100,5 @@ watch(
       </div>
     </div>
   </section>
-
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-    <path
-      fill="#facc15"
-      fill-opacity="1"
-      d="M0,32L60,48C120,64,240,96,360,106.7C480,117,600,107,720,122.7C840,139,960,181,1080,170.7C1200,160,1320,96,1380,64L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-    ></path>
-  </svg>
+  <Waves />
 </template>
