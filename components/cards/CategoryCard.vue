10addd1c40859ce8457ffd9b3302b84049432633
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-04 15:48:33 +0100

    final

--- components/cards/CategoryCard.vue
+++ components/cards/CategoryCard.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
 const props = defineProps<{
+  index: number;
+  category: string;
   img: string;
   text: string;
 }>();
+
+const emits = defineEmits(['emit-category']);
+
+async function navigateToPage(selectedCategory: object) {
+  await navigateTo({
+    path: selectedCategory.category,
+    query: { index: selectedCategory.index },
+  });
+}
 </script>
 
 <template>
   <article
     class="flex flex-col items-center transform transition-transform duration-300 hover:scale-105 cursor-pointer"
+    @click="navigateToPage(props)"
   >
-    <img :src="props.img" :alt="props.text" class="w-20 h-20 rounded-full object-cover" />
+    <img
+      :src="props.img"
+      :alt="props.text"
+      class="w-20 h-20 rounded-full object-cover hover:scale-105"
+    />
 
-    <h3 class="mt-3 text-center text-sm">{{ props.text }}</h3>
+    <h3 class="mt-3 text-center">{{ props.text }}</h3>
   </article>
 </template>
