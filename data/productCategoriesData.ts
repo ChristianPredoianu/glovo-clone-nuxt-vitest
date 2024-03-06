0fcf9c779916b654f58e31382ec5ec4d62917fe8
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-06 13:58:28 +0100

    work

--- data/productCategoriesData.ts
+++ data/productCategoriesData.ts
@@ -1,14 +1,14 @@
 import type { IProductCategories } from '@/interfaces/productCategories.interface';
 
 export const productCategories: IProductCategories[] = [
-  { category: 'generic-foods', text: 'Generic Foods', img: 'generic-food.jpg' },
+  { category: 'Breakfast', text: 'Breakfast', img: 'generic-food.jpg' },
   {
-    category: 'packaged-food',
-    text: 'Packaged Foods',
+    category: 'Lunch',
+    text: 'Lunch',
     img: 'packaged-food.jpg',
   },
-  { category: 'generic-meals', text: 'Generic Meals', img: 'generic-meals.jpg' },
-  { category: 'fast-foods', text: 'Fast Foods', img: 'fast-food.jpg' },
+  { category: 'Dinner', text: 'Dinner', img: 'generic-meals.jpg' },
+  { category: 'Snack', text: 'Snacks', img: 'fast-food.jpg' },
   { category: 'electronics', text: 'Electronics', img: 'electronics.jpg' },
   { category: 'jewelery', text: 'Jewelery', img: 'jewelery.jpg' },
   {
