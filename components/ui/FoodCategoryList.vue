151bf6830557f28261e4a36cae54426814c4c6cb
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-06 15:32:47 +0100

    added component

---
+++ components/ui/FoodCategoryList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  dishTypes: {
+    id: number;
+    dishType: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <ul class="flex flex-wrap gap-4 justify-around">
+    <li
+      class="bg-orange-200 text-lg font-semibold p-4 rounded-3xl cursor-pointer"
+      v-for="dishType in dishTypes"
+      :key="dishType.id"
+    >
+      {{ dishType.dishType.charAt(0).toUpperCase() + dishType.dishType.slice(1) }}
+    </li>
+  </ul>
+</template>
