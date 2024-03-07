dce939a6cb77cdeb65b4e4fa9b27f742e49e09d6
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-07 12:58:09 +0100

    moved

---
+++ components/cards/CategoryCard/CategoryCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface ISelectedCategory {
+  index: number;
+  img: string;
+  text: string;
+  category: string;
+}
+
+const props = defineProps<ISelectedCategory>();
+
+async function navigateToPage(selectedCategory: ISelectedCategory) {
+  await navigateTo({
+    path: selectedCategory.category,
+    query: { index: selectedCategory.index },
+  });
+}
+</script>
+
+<template>
+  <article
+    class="flex flex-col items-center transform cursor-pointer"
+    @click="navigateToPage(props)"
+  >
+    <img
+      :src="props.img"
+      :alt="props.text"
+      class="w-20 h-20 rounded-full object-cover shadow-lg transition-transform duration-300 hover:scale-105"
+    />
+
+    <h3 class="mt-3 text-center font-semibold">{{ props.text }}</h3>
+  </article>
+</template>
---
+++ components/cards/InfoCard/InfoCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  icon: string;
+  heading: string;
+  paragraph: string;
+  span: string;
+}>();
+</script>
+
+<template>
+  <div class="flex justify-around">
+    <article
+      class="flex flex-col items-center gap-4 py-8 px-12 h-full w-1/2 md:w-full text-center"
+    >
+      <font-awesome-icon :icon="['fas', props.icon]" class="text-7xl text-orange-400" />
+      <h3 class="flex-grow text-xl font-bold">{{ props.heading }}</h3>
+      <p class="flex-grow lg:px-10">
+        {{ props.paragraph
+        }}<span class="bg-orange-400 font-medium rounded-md">{{ props.span }}</span>
+      </p>
+    </article>
+  </div>
+</template>
