ed28e7f9ce1df35151c537eb2a69d50d2312cf4f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-07 14:15:40 +0100

    moved

---
+++ components/ui/Backdrop/Backdrop.nuxt.test.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import Backdrop from '@/components/ui/Backdrop/Backdrop.vue';
+import { useBackdrop } from '@/composables/useBackdrop';
+
+/* describe('Backdrop', () => {
+  it('should be closed', async () => {
+    const { isBackdropOpen } = useBackdrop();
+    expect(isBackdropOpen.value).toBe(false);
+  });
+
+  it('should be opened when openBackdrop is called', async () => {
+    const { isBackdropOpen, openBackdrop } = useBackdrop();
+    openBackdrop();
+    expect(isBackdropOpen.value).toBe(true);
+  });
+
+  it('should be closed when closeBackdrop is called', async () => {
+    const { isBackdropOpen, closeBackdrop } = useBackdrop();
+    closeBackdrop();
+    expect(isBackdropOpen.value).toBe(false);
+  });
+}); */
+
+describe('Backdrop', () => {
+  it('should be closed', async () => {
+    const { isBackdropOpen } = useBackdrop();
+    expect(isBackdropOpen.value).toBe(false);
+  });
+
+  it('should be opened when openBackdrop is called', async () => {
+    const { isBackdropOpen, openBackdrop } = useBackdrop();
+    openBackdrop();
+    expect(isBackdropOpen.value).toBe(true);
+  });
+
+  it('should be closed when closeBackdrop is called', async () => {
+    const { isBackdropOpen, closeBackdrop } = useBackdrop();
+    closeBackdrop();
+    expect(isBackdropOpen.value).toBe(false);
+  });
+});
