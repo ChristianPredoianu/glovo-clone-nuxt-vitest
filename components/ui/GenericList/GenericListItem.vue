0552594e0f64a1f16f913e07b6f46464c80cba0f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-08 13:50:01 +0100

    added list

---
+++ components/ui/GenericList/GenericListItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  items: Record<string, any>[];
+  field: string;
+}>();
+
+console.log(props.field);
+</script>
+
+<template>
+  <li
+    class="bg-orange-200 text-lg font-semibold p-4 rounded-3xl cursor-pointer"
+    v-for="(item, key) in items"
+    :key="key"
+  >
+    <!-- {{ item[props.field].charAt(0).toUpperCase() + item[props.field].slice(1) }} -->
+    {{ item.name.common }}
+
+    {{ item[props.field] }}
+  </li>
+</template>
