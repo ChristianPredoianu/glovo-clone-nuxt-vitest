fc6b56c3930b59dd2536f7df1e87fcaa8f0fe84a
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-08 13:49:46 +0100

    added btn

---
+++ components/buttons/CtaBtn.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const emits = defineEmits(['clicked']);
+</script>
+
+<template>
+  <button
+    class="bg-green-600 py-3 px-10 rounded-xl text-gray-100 font-bold cursor-pointer hover:bg-green-700"
+    @click="emits('clicked')"
+  >
+    <slot></slot>
+  </button>
+</template>
