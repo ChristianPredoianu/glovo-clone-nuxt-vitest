094dce5325335d88ab40e467d0b4fbe99b7f1679
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-12 14:13:35 +0100

    added modals

---
+++ components/modals/MealCategoryFilterOverlay.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { cuisineTypes } from '@/data/productCategoriesData';
+</script>
+
+<template>
+  <ul>
+    <li v-for="cuisineType in cuisineTypes" :key="cuisineType.id">
+      <div>
+        <font-awesome-icon :icon="['fas', cuisineType.icon]" />
+        <p>{{ cuisineType.cuisineType }}</p>
+      </div>
+    </li>
+  </ul>
+</template>
