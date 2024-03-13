1affcea63bafd30a26db11d4066c8922e2ead227
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-13 12:22:52 +0100

    fixed wront implementation

--- components/modals/MealCategoryFilterModal.vue
+++ components/modals/MealCategoryFilterModal.vue
@@ -1,6 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { closeModal } = useModal();
+
+function closeModalDialog() {
+  closeModal();
+}
+</script>
 
 <template>
-  <Backdrop />
-  <MealCategoryFilterOverlay />
+  <main class="absolute inset-0 mx-auto w-1/2 h-full z-50 bg-red-500">
+    <Backdrop @closeElement="closeModalDialog" />
+    <MealCategoryFilterOverlay />
+  </main>
 </template>
