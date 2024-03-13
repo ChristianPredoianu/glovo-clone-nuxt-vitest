615c0abb9d1e751ac9b598563337953c84a483bf
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-13 14:37:45 +0100

    work

--- components/buttons/CtaBtn.vue
+++ components/buttons/CtaBtn.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+const props = defineProps({
+  backCol: String,
+  textCol: {
+    type: String,
+    default: undefined,
+  },
+  borderCol: {
+    type: String,
+    default: undefined,
+  },
+  borderWidth: {
+    type: String,
+    default: undefined,
+  },
+});
 const emits = defineEmits(['clicked']);
 </script>
 
 <template>
   <button
-    class="bg-green-600 py-3 px-10 rounded-xl text-gray-100 font-bold cursor-pointer hover:bg-green-700"
+    class="bg-green-600 py-3 px-10 rounded-xl font-bold cursor-pointer hover:bg-green-700"
+    :class="[props.backCol, props.textCol, props.borderCol, props.borderWidth]"
     @click="emits('clicked')"
   >
     <slot></slot>
