874b70707707bba1c81fec8d4c55f65c464570a9
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-13 14:37:55 +0100

    work

--- components/modals/MealCategoryFilterModal.vue
+++ components/modals/MealCategoryFilterModal.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+const emits = defineEmits(['emitSelected']);
+
 const { closeModal } = useModal();
 
 function closeModalDialog() {
   closeModal();
 }
+
+function emitSelected(selectedCuisineType: string) {
+  emits('emitSelected', selectedCuisineType);
+  console.log(selectedCuisineType);
+}
 </script>
 
 <template>
-  <main class="absolute inset-0 mx-auto w-1/2 h-full z-50 bg-red-500">
+  <main
+    class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center z-50 w-full h-full md:h-auto md:w-4/5 bg-white"
+  >
     <Backdrop @closeElement="closeModalDialog" />
-    <MealCategoryFilterOverlay />
+    <MealCategoryFilterOverlay @emitSelected="emitSelected" />
   </main>
 </template>
