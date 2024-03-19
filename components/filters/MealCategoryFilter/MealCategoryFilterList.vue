ac8a7de7bc3a91df1cb2cf3051e38752117f40c6
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-19 14:02:02 +0100

    work

--- components/filters/MealCategoryFilter/MealCategoryFilterList.vue
+++ components/filters/MealCategoryFilter/MealCategoryFilterList.vue
@@ -1,3 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { cuisineTypes } from '@/data/productCategoriesData';
+</script>
 
-<template></template>
+<template>
+  <ul class="flex flex-col gap-y-2">
+    <MealCategoryFilterListItem
+      v-for="cuisineType in cuisineTypes"
+      :key="cuisineType.id"
+      :cuisineType="cuisineType"
+    />
+  </ul>
+</template>
