dbb257011c2162145c1c4979744d07dc3e60dd6c
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-19 14:01:52 +0100

    work

--- components/filters/MealCategoryFilter/MealCategoryFilterListItem.vue
+++ components/filters/MealCategoryFilter/MealCategoryFilterListItem.vue
@@ -1,3 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  cuisineType: Object,
+});
+</script>
 
-<template></template>
+<template>
+  <li class="flex gap-x-2 items-center pb-2 border-solid border-b-1 border-gray-100">
+    <font-awesome-icon
+      :icon="['fas', cuisineType?.icon]"
+      class="bg-orange-200 p-2 rounded-full text-gray-600 cursor-pointer"
+    />
+    <p>{{ cuisineType?.cuisineType }}</p>
+  </li>
+</template>
