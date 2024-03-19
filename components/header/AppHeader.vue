e58e6f19231c3fbb698ce7c447952d53cb2424df
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-19 19:48:52 +0100

    work

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -26,7 +26,7 @@ onUnmounted(() => {
       <div
         id="nav-list-div"
         :class="isNavOpen ? ['translate-x-[0rem]'] : ['-translate-x-[50rem]']"
-        class="h-full bg-white sm:bg-transparent absolute sm:relative z-40 top-0 left-0 w-10/12 sm:w-auto min-h-screen sm:min-h-full flex flex-grow flex-col items-center justify-center sm:justify-start sm:flex-row rounded-r-2xl sm:rounded-r-none transition ease-in-out sm:py-0 sm:translate-x-0 sm:ml-4"
+        class="h-full bg-white sm:bg-transparent absolute sm:relative z-50 top-0 left-0 w-10/12 sm:w-auto min-h-screen sm:min-h-full flex flex-grow flex-col items-center justify-center sm:justify-start sm:flex-row rounded-r-2xl sm:rounded-r-none transition ease-in-out sm:py-0 sm:translate-x-0 sm:ml-4"
       >
         <NavItems />
       </div>
