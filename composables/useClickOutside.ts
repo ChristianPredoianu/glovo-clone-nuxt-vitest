0a88177e569ddb1b265f1710136594c6011fa064
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-21 18:34:51 +0100

    changed to function keyword + refactored

--- composables/useClickOutside.ts
+++ composables/useClickOutside.ts
@@ -2,11 +2,9 @@ export function useClickOutside(
   elementRef: Ref<HTMLElement | null>,
   callback: () => void
 ) {
-  const handleClick = (event: MouseEvent) => {
-    if (elementRef.value && !elementRef.value.contains(event.target as Node)) {
-      callback();
-    }
-  };
+  function handleClick(event: MouseEvent) {
+    if (elementRef.value && !elementRef.value.contains(event.target as Node)) callback();
+  }
 
   onMounted(() => {
     document.addEventListener('click', handleClick);
