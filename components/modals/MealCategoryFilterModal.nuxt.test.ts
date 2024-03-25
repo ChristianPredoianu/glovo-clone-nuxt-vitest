a12dd4070707878ba3dda4491f06352668ab1811
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-25 15:43:58 +0100

    work

---
+++ components/modals/MealCategoryFilterModal.nuxt.test.ts
@@ -0,0 +1,11 @@
+import { describe, vi, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import MealCategoryFilterModal from '@/components/modals/MealCategoryFilterModal.vue';
+import MealCategoryFilterModalOverlay from '@/components/modals/MealCategoryFilterOverlay.vue';
+import { shallowMount, VueWrapper } from '@vue/test-utils';
+
+describe('MealCategoryFilterModal', () => {
+  it('Should display backdrop and meal category filter overlay', async () => {});
+
+  it('Should close modal when screen width is >= 1024', async () => {});
+});
