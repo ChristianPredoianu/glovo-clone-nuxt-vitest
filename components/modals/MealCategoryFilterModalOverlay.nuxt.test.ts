b9daad1d34281ca18035c14f37c9b4c6efd9eac6
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-26 11:25:47 +0100

    work

--- components/modals/MealCategoryFilterModalOverlay.nuxt.test.ts
+++ components/modals/MealCategoryFilterModalOverlay.nuxt.test.ts
@@ -1,16 +1,28 @@
-import { describe, vi, it, expect } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { describe, beforeEach, vi, it, expect } from 'vitest';
+import { mount, VueWrapper } from '@vue/test-utils';
 
 import MealCategoryFilterModalOverlay from '@/components/modals/MealCategoryFilterOverlay.vue';
 
 describe('MealCategoryFilterModalOverlay', () => {
-  it('Should emit', async () => {
-    const wrapper = mount(MealCategoryFilterModalOverlay);
+  let wrapper: VueWrapper<any>;
 
+  beforeEach(() => {
+    wrapper = mount(MealCategoryFilterModalOverlay);
+  });
+
+  it('Should emit', async () => {
     const applyBtn = wrapper.find('[data-test="apply-btn"]');
 
     await applyBtn.trigger('click');
   });
 
-  it('Should close modal when screen width is >= 1024', async () => {});
+  it('Should be active class when click on filter', async () => {
+    const filterListItem = wrapper.find('li');
+
+    await filterListItem.trigger('click');
+
+    const filterIcon = wrapper.find('[data-test="filter-icon"]');
+
+    expect(filterIcon.classes()).toContain('bg-orange-200');
+  });
 });
