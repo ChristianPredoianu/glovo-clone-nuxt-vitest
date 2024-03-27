eefe9f339fe376e585e81ff4e61fc41c2253f49c
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-27 12:37:33 +0100

    work

--- components/ui/GenericList/GenericList.nuxt.test.ts
+++ components/ui/GenericList/GenericList.nuxt.test.ts
@@ -1,29 +1,32 @@
 import { describe, it, expect } from 'vitest';
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import GenericList from '@/components/ui/GenericList/GenericList.vue';
 
-describe('AdressForm', () => {
-  it('should renders dish types correctly', async () => {
-    const dishTypes = [
-      { id: 1, dishType: 'salad' },
-      { id: 2, dishType: 'soup' },
-      { id: 3, dishType: 'dessert' },
+describe('GenericList', () => {
+  it('renders list items correctly', () => {
+    const items = [
+      { id: 1, name: 'Item 1' },
+      { id: 2, name: 'Item 2' },
+      { id: 3, name: 'Item 3' },
     ];
 
-    const wrapper = shallowMount(GenericList, {
+    const field = 'name';
+
+    const wrapper = mount(GenericList, {
       props: {
-        dishTypes,
+        items,
+        field,
       },
     });
 
     const listItems = wrapper.findAll('li');
 
-    expect(listItems.length).toBe(dishTypes.length);
+    expect(listItems.length).toBe(items.length);
 
-    dishTypes.forEach((dishType, index) => {
-      expect(listItems[index].text()).toContain(
-        dishType.dishType.charAt(0).toUpperCase() + dishType.dishType.slice(1)
-      );
+    listItems.forEach((listItem, index) => {
+      const itemText = listItem.text();
+
+      expect(itemText).toContain(items[index][field]);
     });
   });
 });
