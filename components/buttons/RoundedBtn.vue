d5ba13a5d5618dd9c8f6ce21e6fef3d2ab2ef083
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-28 12:10:26 +0100

    extracted btn

---
+++ components/buttons/RoundedBtn.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+  icon: string;
+  backCol: string;
+}>();
+
+const emits = defineEmits(['emitClick']);
+
+function handleClick() {
+  emits('emitClick');
+}
+</script>
+
+<template>
+  <font-awesome-icon
+    :icon="['fas', props.icon]"
+    class="bg-orange-200 p-2 rounded-full text-gray-600 cursor-pointer"
+    :class="[props.backCol]"
+    @click="handleClick"
+  />
+  <p class="text-xs text-center">{{ props.text }}</p>
+</template>
