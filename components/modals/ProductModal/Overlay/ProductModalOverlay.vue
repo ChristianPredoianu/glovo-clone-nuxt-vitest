69f84e13bc2adfbe07ac1aa34b33e762721c6db6
Christian Predoianu <christianpredoianu@yahoo.com>
2024-03-29 13:39:45 +0100

    work

---
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="container mx-auto py-10 px-4 flex flex-col overflow-auto">
+    <h1>this is p modal</h1>
+  </main>
+</template>
---
+++ components/modals/ProductModal/ProductModal.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main
+    class="modal fixed top-0 flex flex-col justify-center items-center z-50 w-full h-full bg-white"
+  >
+    <Backdrop />
+    <ProductModalOverlay />
+  </main>
+</template>
