391f1dcdfb226906e12175e0c4b7a9392b8f229f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-04-05 14:49:25 +0200

    fixed modals

---
+++ components/modals/Modal/Modal.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const dialogRef = ref<HTMLDialogElement | null>(null);
+
+const showDialog = () => dialogRef.value?.showModal();
+const closeDialog = () => dialogRef.value?.close();
+
+defineExpose({
+  showDialog,
+  closeDialog,
+});
+</script>
+
+<template>
+  <Teleport to="body">
+    <dialog ref="dialogRef" class="w-full h-full">
+      <slot />
+    </dialog>
+  </Teleport>
+</template>
