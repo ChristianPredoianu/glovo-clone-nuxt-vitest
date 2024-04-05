866ddfcd80ac43fd7cdabe426f268e342750e7a4
Christian Predoianu <christianpredoianu@yahoo.com>
2024-04-05 15:29:54 +0200

    removed section

--- components/modals/ProductModal/ProductModal.vue
+++ components/modals/ProductModal/ProductModal.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main
-    class="modal fixed top-0 flex flex-col justify-center items-center z-50 w-full h-full bg-white"
-  >
+  <main class="fixed top-0 flex flex-col justify-center items-center z-50 w-1/2 bg-white">
     <Backdrop />
     <ProductModalOverlay />
   </main>
