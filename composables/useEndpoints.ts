b4870de11c368b58a4c5e18e5a51396d908c671f
Christian Predoianu <christianpredoianu@yahoo.com>
2024-04-18 16:58:16 +0200

    added endpoints

--- composables/useEndpoints.ts
+++ composables/useEndpoints.ts
@@ -1,16 +1,28 @@
 import { fakeStoreCategories } from '@/data/productCategoriesData';
 
 export function useEndpoints(
-  category: string | string[],
-  filter: Ref<string>,
-  routeIndex: string
+  category?: string | string[],
+  filter?: Ref<string>,
+  routeIndex?: string
 ) {
   const runtimeConfig = useRuntimeConfig();
 
+  const restCountriesEndpoint = 'https://restcountries.com/v3.1/region/europe';
+
+  const indexMealDataEndpoint = computed(() => {
+    return `${runtimeConfig.public.apiEdamam}&app_id=e5a7e476&app_key=4b4dc5f4bc65e69c3e05af0392a55b18%09&mealType=Dinner&dishType=Main%20course`;
+  });
+
+  const locationEndpoint = computed(() => {
+    if (filter)
+      return `${runtimeConfig.public.apiAutoconfig}pk.a75cdfe1cc307b34218d8021f4122dc6&q=${filter.value}&limit=5`;
+  });
+
   const initialFetchEndpoint = computed(() => {
-    return routeIndex !== null && +routeIndex >= 0 && +routeIndex <= 3
-      ? edamamApiEndpoint.value
-      : fakeStoreEndpoint.value;
+    if (routeIndex)
+      return routeIndex !== null && +routeIndex >= 0 && +routeIndex <= 3
+        ? edamamApiEndpoint.value
+        : fakeStoreEndpoint.value;
   });
 
   const edamamApiEndpoint = computed(() => {
@@ -18,7 +30,8 @@ export function useEndpoints(
   });
 
   const edamamApiFilteredEndpoint = computed(() => {
-    return `${runtimeConfig.public.apiEdamam}&app_id=e5a7e476&app_key=4b4dc5f4bc65e69c3e05af0392a55b18%09&mealType=${category}&cuisineType=${filter.value}`;
+    if (filter)
+      return `${runtimeConfig.public.apiEdamam}&app_id=e5a7e476&app_key=4b4dc5f4bc65e69c3e05af0392a55b18%09&mealType=${category}&cuisineType=${filter.value}`;
   });
 
   const fakeStoreEndpoint = computed(() => {
@@ -26,13 +39,18 @@ export function useEndpoints(
   });
 
   const fakeStoreFilteredEndpoint = computed(() => {
-    return `${runtimeConfig.public.fakeStoreBase}${filter.value.toLowerCase()}`;
+    if (filter)
+      return `${runtimeConfig.public.fakeStoreBase}${filter.value.toLowerCase()}`;
   });
 
   const selectedApiEndpoint = computed(() => {
-    const shouldFetchFromFakeStore = fakeStoreCategories.some(
-      (category) => category.category.toLowerCase() === filter.value.toLowerCase()
-    );
+    let shouldFetchFromFakeStore;
+
+    if (filter) {
+      shouldFetchFromFakeStore = fakeStoreCategories.some(
+        (category) => category.category.toLowerCase() === filter.value.toLowerCase()
+      );
+    }
 
     return shouldFetchFromFakeStore
       ? fakeStoreFilteredEndpoint.value
@@ -40,6 +58,9 @@ export function useEndpoints(
   });
 
   return {
+    indexMealDataEndpoint,
+    locationEndpoint,
+    restCountriesEndpoint,
     initialFetchEndpoint,
     selectedApiEndpoint,
   };
