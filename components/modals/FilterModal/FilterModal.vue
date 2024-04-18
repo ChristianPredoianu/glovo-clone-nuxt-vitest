6870b12affff1a286a30834e8a3bda8f1c158f50
Christian Predoianu <christianpredoianu@yahoo.com>
2024-04-18 15:57:18 +0200

    work

--- components/modals/FilterModal/FilterModal.vue
+++ components/modals/FilterModal/FilterModal.vue
@@ -7,9 +7,7 @@ function emitSelected(selectedCuisineType: string) {
 </script>
 
 <template>
-  <main
-    class="modal fixed top-0 flex flex-col justify-center items-center z-50 w-full h-full bg-white"
-  >
+  <main class="modal">
     <Backdrop />
     <FilterModalOverlay @emitSelected="emitSelected" />
   </main>
