771206d127e494db7bf9cd603181071350a8c621
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-02 14:48:26 +0200

    final

--- composables/useIsActive.ts
+++ composables/useIsActive.ts
@@ -1,15 +1,17 @@
 export function useIsActive() {
   const selected = useState<string | number>('selected', () => '');
 
-  function toggleActive(userSelected: string | number) {
-    selected.value === userSelected
-      ? (selected.value = '')
-      : (selected.value = userSelected);
+  function setActive(userSelected: string | number) {
+    selected.value = userSelected;
   }
 
   function isActive(userSelected: string | number): boolean {
     return selected.value === userSelected;
   }
 
-  return { selected, toggleActive, isActive };
+  function clearActive() {
+    selected.value = '';
+  }
+
+  return { setActive, isActive, clearActive };
 }
