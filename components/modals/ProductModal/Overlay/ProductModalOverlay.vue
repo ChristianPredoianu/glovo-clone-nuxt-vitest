62511b6ceab9c03b86303e836563c3eb4ffff086
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-02 13:27:59 +0200

    work

--- components/modals/ProductModal/Overlay/ProductModalOverlay.vue
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface ProductModalProps {
+  label: string;
+  img: string;
+}
+
+const props = defineProps<{
+  productModalProps: ProductModalProps | null;
+}>();
+</script>
 
 <template>
   <main class="container mx-auto py-10 px-4 flex flex-col overflow-auto">
-    <h1>this is p modal</h1>
+    <h1 v-if="productModalProps">{{ props.productModalProps!.label }}</h1>
+    <!--     <img :src="props.img" :alt="props.label" /> -->
+    <h1 v-if="props">{{ props }}</h1>
   </main>
 </template>
