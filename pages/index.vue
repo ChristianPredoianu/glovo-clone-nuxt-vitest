38119a9a2da45333f325129c199b569e18849297
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-04 11:42:25 +0200

    changed ref to useState

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,10 @@ const emittedOption = useState<IDropdownOptions>('emittedOption', () => {
   return { id: 0, text: '' };
 });
 
-const productDialog = ref<InstanceType<typeof Modal> | null>(null);
+const productDialog = useState<InstanceType<typeof Modal> | null>(
+  'filterDialog',
+  () => null
+);
 
 const emittedLocation = useState<ILocationAdress>('emittedLocation', () => {
   return { address: { road: '', postcode: '', town: '', country: '' } };
