87937b8021c532bac4631851b7f7a5839ebd53ed
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-04 15:46:01 +0200

    deleted

--- components/modals/ProductModal/ProductModal.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main class="fixed top-0 flex flex-col justify-center items-center z-50 w-1/2 bg-white">
-    <Backdrop />
-    <ProductModalOverlay />
-  </main>
-</template>
