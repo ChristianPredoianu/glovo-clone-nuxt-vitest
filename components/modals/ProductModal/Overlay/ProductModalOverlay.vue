dcb4ac5eed56f6b78901f0da7cb49e1502a25f04
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-04 15:46:21 +0200

    work

--- components/modals/ProductModal/Overlay/ProductModalOverlay.vue
+++ components/modals/ProductModal/Overlay/ProductModalOverlay.vue
@@ -1,28 +1,44 @@
 <script setup lang="ts">
-import type { ModalProps } from '@/interfaces/interfaces.interface';
-import { generateRandomPrice } from '@/helpers/randomPrice';
+import type { ModalProps, IMealModalProps } from '@/interfaces/interfaces.interface';
 
 const props = defineProps<{ productModalProps: ModalProps | null; price: number }>();
 
-const price = ref<string | null>(null);
+const emits = defineEmits(['closeModal']);
 
-onMounted(() => {
-  price.value = generateRandomPrice();
-});
+const isMealModalProps = (props: ModalProps | null): props is IMealModalProps => {
+  return props !== null && 'ingredients' in props;
+};
 </script>
 
 <template>
   <main
-    class="container mx-auto py-10 px-4 flex flex-col overflow-auto"
+    class="container relative mx-auto p-10 flex flex-col overflow-auto"
     v-if="productModalProps"
   >
-    <div class="">
+    <p
+      class="text-xl text-right mb-8 font-semibold cursor-pointer"
+      @click="emits('closeModal')"
+    >
+      Close
+    </p>
+    <div class="mx-auto">
       <img :src="props.productModalProps!.img" :alt="props.productModalProps!.label" />
     </div>
     <h1 class="text-xl font-semibold text-gray-700 py-4">
       {{ props.productModalProps!.label.replace(/\brecipe\b|\brecipes\b/gi, '').trim() }}
     </h1>
-
-    <h1>{{ props.price }}$</h1>
+    <h2 class="text-red-500 font-semibold text-2xl">{{ props.price }}$</h2>
+    <div
+      class="mt-2 mb-8 gap-2 text-gray-500"
+      v-if="isMealModalProps(props.productModalProps)"
+    >
+      <ul
+        v-for="ingredient in props.productModalProps!.ingredients"
+        :key="ingredient.text"
+      >
+        <li>{{ ingredient.text }}</li>
+      </ul>
+    </div>
+    <CtaBtn :textCol="'text-gray-200'">Add to cart {{ price }} $</CtaBtn>
   </main>
 </template>
