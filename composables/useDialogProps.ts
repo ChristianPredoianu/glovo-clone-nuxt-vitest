959ab5db30b1bc3d280e6918b76bb95b3cce6b33
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-06 15:25:04 +0200

    removed console.log

--- composables/useDialogProps.ts
+++ composables/useDialogProps.ts
@@ -20,9 +20,7 @@ export function useDialogProps(dialogRef: IModalRef) {
   const { isSingleMealData } = useIsMealData();
 
   function handleCardClick(item: ISingleMeal | IProduct) {
-    console.log(item);
     if (isSingleMealData(item)) {
-      console.log(item);
       mealModalProps.value = {
         label: item.recipe.label,
         img: item.recipe.image,
