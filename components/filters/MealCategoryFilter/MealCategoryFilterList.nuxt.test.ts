1affd50ae9c17e3710c898b64fd9837d531087b0
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-31 14:59:32 +0200

    fixed the test

--- components/filters/MealCategoryFilter/MealCategoryFilterList.nuxt.test.ts
+++ components/filters/MealCategoryFilter/MealCategoryFilterList.nuxt.test.ts
@@ -1,24 +1,52 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, vi, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import MealCategoryFilterList from '@/components/filters/MealCategoryFilter/MealCategoryFilterList.vue';
+import MealCategoryFilterList from './MealCategoryFilterList.vue';
+import MealCategoryFilterListItem from './MealCategoryFilterListItem.vue';
+import type {
+  IFakeStoreCategories,
+  ICuisineType,
+} from '@/interfaces/interfaces.interface';
 
-describe('MealCateGoryFilterList', () => {
-  it('should emits selected category when clicked', async () => {
-    const filters = [
-      { id: 1, cuisineType: 'American', icon: 'fa-burger' },
-      { id: 2, cuisineType: 'Asian', icon: 'fa-bowl-food' },
-      { id: 3, cuisineType: 'British', icon: 'fa-bowl-rice' },
-    ];
+vi.mock('@/composables/useFilter', () => ({
+  useFilter: () => ({
+    filters: [
+      { id: 1, icon: 'fa-burger', cuisineType: 'American' },
+      { id: 2, icon: 'fa-pizza', cuisineType: 'Pizza' },
+    ],
+    getCategoryName: (category: IFakeStoreCategories | ICuisineType) => {
+      return 'cuisineType' in category ? category.cuisineType : category.category;
+    },
+  }),
+}));
 
+describe('MealCategoryFilterList', () => {
+  it('renders the correct number of MealCategoryFilterListItem components', () => {
     const wrapper = mount(MealCategoryFilterList, {
-      props: {
-        filters: filters,
+      global: {
+        components: {
+          MealCategoryFilterListItem,
+        },
       },
     });
 
-    await wrapper.findComponent({ name: 'MealCategoryFilterListItem' }).trigger('click');
+    const listItems = wrapper.findAllComponents(MealCategoryFilterListItem);
+
+    expect(listItems).toHaveLength(2);
+  });
+
+  it('emits the emitSelected event when a MealCategoryFilterListItem emits it', async () => {
+    const wrapper = mount(MealCategoryFilterList, {
+      global: {
+        components: {
+          MealCategoryFilterListItem,
+        },
+      },
+    });
+
+    const listItem = wrapper.findComponent(MealCategoryFilterListItem);
+    await listItem.vm.$emit('emitSelected', 'American');
 
     expect(wrapper.emitted('emitSelected')).toBeTruthy();
-    expect(wrapper.emitted('emitSelected')![0][0]).toBe('American');
+    expect(wrapper.emitted('emitSelected')![0]).toEqual(['American']);
   });
 });
