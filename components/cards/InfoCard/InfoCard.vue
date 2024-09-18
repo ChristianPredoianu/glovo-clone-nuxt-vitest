07f9c3d027cde2e97da584b0e6d71fda1c6f94e6
Christian Predoianu <christianpredoianu@yahoo.com>
2024-09-18 15:01:59 +0200

    work

--- components/cards/InfoCard/InfoCard.vue
+++ components/cards/InfoCard/InfoCard.vue
@@ -16,7 +16,9 @@ const props = defineProps<{
       <h3 class="flex-grow text-xl font-bold">{{ props.heading }}</h3>
       <p class="flex-grow lg:px-10">
         {{ props.paragraph
-        }}<span class="bg-orange-400 font-medium rounded-md">{{ props.span }}</span>
+        }}<span class="bg-orange-400 font-medium rounded-md px-2 py-1">{{
+          props.span
+        }}</span>
       </p>
     </article>
   </div>
