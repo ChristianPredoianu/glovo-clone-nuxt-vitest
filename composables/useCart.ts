8d996652870ed149a9dc1ad15f144c27e4115dee
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-02 11:17:11 +0200

    fixed product interface

--- composables/useCart.ts
+++ composables/useCart.ts
@@ -1,6 +1,8 @@
+import type { ICartProduct } from '@/interfaces/interfaces.interface';
+
 export function useCart() {
-  function addToCart(product) {
-    console.log(product.value);
+  function addToCart(product: ICartProduct | null) {
+    console.log(product);
   }
 
   return { addToCart };
