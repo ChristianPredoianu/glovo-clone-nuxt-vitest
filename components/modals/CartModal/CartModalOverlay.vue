25ca727da4c199b45415fed43f19c91c23aa4c84
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-02 15:45:18 +0200

    work

--- components/modals/CartModal/CartModalOverlay.vue
+++ components/modals/CartModal/CartModalOverlay.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { numberOfCartProducts } = useCart();
+</script>
 
 <template>
-  <h1>CartModal</h1>
+  <section
+    class="container mx-auto flex flex-col items-center justify-center gap-10 p-10"
+  >
+    <h3 class="text-xl font-bold">Your order</h3>
+    <div
+      class="flex flex-col items-center justify-center gap-10"
+      v-if="numberOfCartProducts === 0"
+    >
+      <img src="@/public/empty-cart.svg" alt="empty cart" class="max-w-xs max-h-48" />
+      <h4 class="text-lg text-center font-semibold">
+        You've not added any products yet. When you do, you'll see them here!
+      </h4>
+    </div>
+    <div
+      class="w-full bg-blue-500 flex flex-col items-center justify-center gap-2"
+      v-else
+    >
+      <CartItemList />
+    </div>
+  </section>
 </template>
