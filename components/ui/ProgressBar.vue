6d8ae3d8041c4a24855afc0923abcb974856e6e4
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-02 13:08:22 +0200

    added progressBar component

---
+++ components/ui/ProgressBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { progressWidth, isBarActive } = useProgressBar();
+</script>
+
+<template>
+  <div
+    class="relative w-full h-1 bg-gray-300 rounded-md overflow-hidden"
+    v-if="isBarActive"
+  >
+    <div
+      class="h-full bg-green-500 transition-all duration-300 ease-linear"
+      :style="{ width: progressWidth + '%' }"
+    ></div>
+  </div>
+</template>
