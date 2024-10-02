10116346717c2a0cf075ca07bbe89ad8b6ef4ade
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-02 15:43:47 +0200

    added component

---
+++ components/counters/ProductCounter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex space-between items-center justify-center">
+    <p class="">+</p>
+    <p class="">1</p>
+    <p class="">-</p>
+  </div>
+</template>
