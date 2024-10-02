3c280b8989a44c98ef8075854d9e4fe049d94784
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-02 15:43:57 +0200

    added component

---
+++ components/cart/CartItem/CartItemList.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { cartProducts } = useCart();
+</script>
+
+<template>
+  <ul class="w-full" v-for="product in cartProducts" :key="product.id">
+    <CartItemListItem :product="product" />
+  </ul>
+</template>
---
+++ components/cart/CartItem/CartItemListItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { ICartProduct } from '@/interfaces/interfaces.interface';
+
+const props = defineProps<{
+  product: ICartProduct;
+}>();
+</script>
+
+<template>
+  <div class="flex bg-red-500 justify-around items-center">
+    <img
+      :src="product.img"
+      alt="product image"
+      class="w-16 h-16 rounded-full object-cover"
+    />
+    <p class="text-lg font-bold">1x</p>
+    <p class="text-center w-3/5 bg-blue-500">{{ product.label }}</p>
+    <p class="">{{ product.price }} $</p>
+  </div>
+</template>
