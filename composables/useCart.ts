d7e60f8c8d5c7b95c038f79636848ed9ab435681
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-03 13:08:37 +0200

    work

--- composables/useCart.ts
+++ composables/useCart.ts
@@ -1,7 +1,9 @@
 import type { ICartProduct } from '@/interfaces/interfaces.interface';
 
 export function useCart() {
-  const cartProducts = useState<ICartProduct[]>('error', () => []);
+  const cartProducts = useState<ICartProduct[]>('cartProducts', () => []);
+  const totalPrice = useState<number>('totalPrice', () => 0);
+  const productQuantity = useState<number>('productQuantity', () => 0);
 
   const { startProgressBar } = useProgressBar();
 
@@ -9,12 +11,36 @@ export function useCart() {
 
   function addToCart(product: ICartProduct | null) {
     if (product !== null) {
-      console.log('added');
-      cartProducts.value.push(product);
+      const existingProduct = cartProducts.value.find((p) => p.id === product.id);
+
+      if (existingProduct) {
+        // Product already in the cart, increase quantity
+        existingProduct.quantity = (existingProduct.quantity || 1) + 1;
+        console.log(`Increased quantity for ${existingProduct.name}`);
+      } else {
+        // New product, add to the cart with quantity 1
+        cartProducts.value.push({ ...product, quantity: 1 });
+        console.log('Product added to the cart.');
+      }
+
       startProgressBar();
       console.log(cartProducts.value);
     }
   }
 
-  return { cartProducts, addToCart, numberOfCartProducts };
+  function removeFromCart(productId: number | string) {
+    const productIndex = cartProducts.value.findIndex(
+      (product) => product.id === productId
+    );
+
+    if (productIndex !== -1) cartProducts.value.splice(productIndex, 1);
+
+    console.log(cartProducts.value);
+  }
+
+  function isProductInCart(productId: number | string) {
+    return cartProducts.value.some((product) => product.id === productId);
+  }
+
+  return { cartProducts, addToCart, removeFromCart, numberOfCartProducts };
 }
