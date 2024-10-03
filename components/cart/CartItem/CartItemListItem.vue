525a4744172f86c76b491c7dcd941888454d7e7e
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-03 15:07:20 +0200

    work

--- components/cart/CartItem/CartItemListItem.vue
+++ components/cart/CartItem/CartItemListItem.vue
@@ -12,11 +12,11 @@ const props = defineProps<{
   <li class="flex flex-col gap-2 border-b border-gray-300 py-4">
     <div class="flex justify-around">
       <img :src="product.img" alt="product image" class="w-16 h-16 rounded-xl" />
-      <p class="text-lg font-bold">1x</p>
+      <p class="text-lg font-bold">{{ product.quantity }}x</p>
       <p class="text-center w-3/5">{{ product.label }}</p>
       <p class="">{{ product.price.toFixed(2) }} $</p>
       <p @click="removeFromCart(product.id)">x</p>
     </div>
-    <ProductCounter />
+    <ProductCounter :quantity="product.quantity ?? 0" :product="product" />
   </li>
 </template>
