82892049a7c8883fb6eca49c7566b43639fc72f7
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-03 15:07:11 +0200

    work

--- components/counters/ProductCounter.vue
+++ components/counters/ProductCounter.vue
@@ -1,15 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ICartProduct } from '@/interfaces/interfaces.interface';
+
+const props = defineProps<{
+  quantity: number;
+  product: ICartProduct;
+}>();
+
+const { addToCart, removeFromCart } = useCart();
+</script>
 
 <template>
   <div class="flex space-between items-center justify-center gap-2">
     <p
       class="bg-gray-500 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8"
+      @click="removeFromCart(product.id)"
     >
       -
     </p>
-    <p class="border border-gray-500 py-1 px-3">1</p>
+    <p class="border border-gray-500 py-1 px-3">{{ quantity }}</p>
     <p
       class="bg-gray-400 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8"
+      @click="addToCart(product)"
     >
       +
     </p>
