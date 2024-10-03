47ff4d01d2cdca1332f2772479609282ad089a8b
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-03 13:09:01 +0200

    work

--- components/counters/ProductCounter.vue
+++ components/counters/ProductCounter.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex space-between items-center justify-center">
-    <p class="">+</p>
-    <p class="">1</p>
-    <p class="">-</p>
+  <div class="flex space-between items-center justify-center gap-2">
+    <p
+      class="bg-gray-500 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8"
+    >
+      -
+    </p>
+    <p class="border border-gray-500 py-1 px-3">1</p>
+    <p
+      class="bg-gray-400 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8"
+    >
+      +
+    </p>
   </div>
 </template>
