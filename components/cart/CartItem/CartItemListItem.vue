9c30b023a2560827a73aaa01d7c192e8a95255e2
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-03 13:09:10 +0200

    work

--- components/cart/CartItem/CartItemListItem.vue
+++ components/cart/CartItem/CartItemListItem.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import type { ICartProduct } from '@/interfaces/interfaces.interface';
 
+const { removeFromCart } = useCart();
+
 const props = defineProps<{
   product: ICartProduct;
 }>();
 </script>
 
 <template>
-  <div class="flex bg-red-500 justify-around items-center">
-    <img
-      :src="product.img"
-      alt="product image"
-      class="w-16 h-16 rounded-full object-cover"
-    />
-    <p class="text-lg font-bold">1x</p>
-    <p class="text-center w-3/5 bg-blue-500">{{ product.label }}</p>
-    <p class="">{{ product.price }} $</p>
-  </div>
+  <li class="flex flex-col gap-2 border-b border-gray-300 py-4">
+    <div class="flex justify-around">
+      <img :src="product.img" alt="product image" class="w-16 h-16 rounded-xl" />
+      <p class="text-lg font-bold">1x</p>
+      <p class="text-center w-3/5">{{ product.label }}</p>
+      <p class="">{{ product.price.toFixed(2) }} $</p>
+      <p @click="removeFromCart(product.id)">x</p>
+    </div>
+    <ProductCounter />
+  </li>
 </template>
