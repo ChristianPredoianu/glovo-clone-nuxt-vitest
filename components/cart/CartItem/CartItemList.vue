caca6120fb85e85bc500f7929bf70da36422a457
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-04 15:44:04 +0200

    deleted

--- components/cart/CartItem/CartItemList.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const { cartProducts } = useCart();
-</script>
-
-<template>
-  <ul class="w-full" v-for="product in cartProducts" :key="product.id">
-    <CartItemListItem :product="product" />
-  </ul>
-</template>
