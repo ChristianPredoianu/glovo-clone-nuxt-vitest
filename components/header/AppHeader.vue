995561addbc3ec7b007985c8ad94647ca3252702
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-05 14:03:16 +0200

    work

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -23,14 +23,11 @@ onUnmounted(() => {
 
 <template>
   <header class="relative bg-amber-400" v-if="screenWidth">
-    <nav
-      class="flex bg-red-500 items-center justify-between flex-wrap container mx-auto p-4"
-    >
+    <nav class="flex items-center justify-between flex-wrap container mx-auto p-4">
       <Modal ref="cartDialog"
         ><CartModalOverlay @closeModal="cartDialog?.closeDialog()"
       /></Modal>
       <Logo />
-
       <Backdrop @closeElement="closeNav" />
       <Hamburger />
 
@@ -40,15 +37,19 @@ onUnmounted(() => {
         class="h-full bg-white sm:bg-transparent fixed sm:relative z-40 top-0 left-0 w-10/12 sm:w-auto min-h-screen sm:min-h-full flex flex-grow flex-col items-center justify-center sm:justify-start sm:flex-row rounded-r-2xl sm:rounded-r-none transition ease-in-out sm:py-0 sm:translate-x-0 sm:ml-4"
       >
         <NavItems />
-        <div class="flex items-center justify-center gap-2">
-          <font-awesome-icon
-            :icon="['fas', 'cart-shopping']"
-            v-if="screenWidth > 640"
-            class="cursor-pointer text-xl"
-            @click="cartDialog?.showDialog()"
-          />
-          <CartCounter />
-          <CtaBtn :fontSize="'text-xs'" :paddingX="'px-6'">Log in</CtaBtn>
+        <div class="w-3/5 md:w-1/2 flex items-center justify-end gap-6">
+          <div class="flex items-center gap-1">
+            <font-awesome-icon
+              :icon="['fas', 'cart-shopping']"
+              v-if="screenWidth > 640"
+              class="cursor-pointer text-xl"
+              @click="cartDialog?.showDialog()"
+            />
+            <CartCounter />
+          </div>
+          <CtaBtn :fontSize="'text-xs'" :textCol="'text-gray-100'" :paddingX="'px-6'"
+            >Sign in</CtaBtn
+          >
         </div>
       </div>
     </nav>
