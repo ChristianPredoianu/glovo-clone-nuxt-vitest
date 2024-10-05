ab5d3472bce5a2cdd338574c56432e62d38a3120
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-05 14:03:25 +0200

    work

--- components/cards/CategoryCard/CategoryCard.vue
+++ components/cards/CategoryCard/CategoryCard.vue
@@ -21,11 +21,13 @@ async function navigateToPage(selectedCategory: ISelectedCategory) {
     class="flex flex-col items-center transform cursor-pointer"
     @click="navigateToPage(props)"
   >
-    <img
-      :src="props.img"
-      :alt="props.text"
-      class="w-20 h-20 rounded-full object-cover shadow-lg transition-transform duration-300 hover:scale-105"
-    />
-    <h3 class="text-gray-900 mt-3 text-center font-semibold">{{ props.text }}</h3>
+    <div class="relative w-24 h-24 xl:w-32 xl:h-32 flex items-center justify-center">
+      <img
+        :src="props.img"
+        :alt="props.text"
+        class="w-full shadow-lg shadow-orange-200 h-full object-cover transform transition-transform duration-500 ease-in-out rounded-[40%_60%_70%_30%_/_30%_50%_60%_40%] hover:rounded-[60%_40%_50%_50%_/_50%_30%_40%_60%] hover:scale-105"
+      />
+    </div>
+    <h3 class="text-gray-700 mt-3 text-center font-semibold">{{ props.text }}</h3>
   </article>
 </template>
