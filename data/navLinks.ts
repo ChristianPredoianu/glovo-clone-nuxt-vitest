21f513225d9c696eeaf8aa5dbf3518de4702fdd6
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-05 11:53:23 +0200

    work

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -2,7 +2,7 @@ import type { INavLinks } from '@/interfaces/interfaces.interface';
 
 export const navLinks: INavLinks[] = [
   { title: 'Home', link: '', icon: ['fas', 'house'] },
-  { title: 'Search', link: '', icon: ['fas', 'magnifying-glass'] },
+  /*  { title: 'Search', link: '', icon: ['fas', 'magnifying-glass'] }, */
   { title: 'Cart', link: '', icon: ['fas', 'cart-shopping'] },
   { title: 'My Orders', link: '', icon: ['fas', 'newspaper'] },
   { title: 'My Account', link: '', icon: ['fas', 'face-smile'] },
