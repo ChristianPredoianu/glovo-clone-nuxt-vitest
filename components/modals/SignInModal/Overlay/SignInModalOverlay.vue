78fa3dcc5d22150fd257b99794af5fad321fa028
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-09 13:34:18 +0200

    work

--- components/modals/SignInModal/Overlay/SignInModalOverlay.vue
+++ components/modals/SignInModal/Overlay/SignInModalOverlay.vue
@@ -2,27 +2,28 @@
 import SignInForm from '@/components/forms/SignInForm/SignInForm.vue';
 import SignUpForm from '@/components/forms/SignUpForm/SignUpForm.vue';
 
-const currentComponent = ref(markRaw(SignInForm));
+const currentComponent = ref<typeof SignInForm | typeof SignUpForm>(markRaw(SignInForm));
+const isSignIn = ref<boolean>(true);
 
-function changeToSignUpComp() {
-  currentComponent.value = markRaw(SignUpForm);
+function toggleForm() {
+  isSignIn.value
+    ? (currentComponent.value = markRaw(SignUpForm))
+    : (currentComponent.value = markRaw(SignInForm));
+
+  isSignIn.value = !isSignIn.value;
 }
 </script>
 
 <template>
-  <section class="relative z-40 container mx-auto md:p-16">
-    <h3 class="text-center text-3xl font-bold pb-10">
-      {{ currentComponent === SignInForm ? 'Sign In' : 'Sign Up' }}
-    </h3>
-    <img src="@/sign-in.svg" alt="sign in" class="mx-auto mb-8 w-24 h-24" />
-    <component :is="currentComponent" />
-    <h4 v-if="currentComponent === SignInForm">
-      Dont't have an account yet?
-      <span
-        class="text-yellow-600 font-semibold cursor-pointer"
-        @click="changeToSignUpComp"
-        >Sign up</span
-      >
-    </h4>
+  <section class="relative z-40 container mx-auto px-4">
+    <div class="flex flex-col gap-7 p-4 md:p-10 w-full">
+      <h3 class="text-center text-3xl font-bold py-10">
+        {{ currentComponent === SignInForm ? 'Sign In' : 'Sign Up' }}
+      </h3>
+      <img src="@/sign-in.svg" alt="sign in" class="mx-auto mb-8 w-24 h-24" />
+
+      <component :is="currentComponent" />
+    </div>
+    <button @click="toggleForm">dsadas</button>
   </section>
 </template>
