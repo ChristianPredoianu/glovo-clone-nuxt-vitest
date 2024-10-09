44cd86b42ada57e1496c0f4df3dd57798f4ac4b1
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-09 13:34:52 +0200

    work

--- components/forms/SignInForm/SignInForm.vue
+++ components/forms/SignInForm/SignInForm.vue
@@ -2,6 +2,8 @@
 const userEmail = ref('');
 const userPassword = ref('');
 
+const emits = defineEmits(['emitSelected']);
+
 const { signIn, emailError, signInPasswordError } = useAuth();
 
 function handleSignIn(e: Event) {
@@ -12,7 +14,8 @@ function handleSignIn(e: Event) {
 
 <template>
   <h1>{{ emailError }}</h1>
-  <form class="flex flex-col gap-7 py-10 w-full">
+
+  <form class="flex flex-col gap-7 p-4 w-full">
     <div class="flex flex-col">
       <label for="email" class="text-sm font-medium text-gray-700">Email</label>
       <input
@@ -24,11 +27,7 @@ function handleSignIn(e: Event) {
         class="w-full border-0 border-b-2 border-gray-300 p-2 mt-1 focus:outline-none focus:ring-2 focus:ring-yellow-500"
         placeholder="email@example.com"
       />
-      <p
-        class="text-red-600 text-sm mt-1"
-        :class="{ invisible: !emailError }"
-        style="min-height: 1.5rem"
-      >
+      <p class="text-red-600 text-xs mt-1 h-4" :class="{ invisible: !emailError }">
         {{ emailError || '' }}
       </p>
     </div>
@@ -41,13 +40,12 @@ function handleSignIn(e: Event) {
         name="password"
         autocomplete="current-password"
         required
-        class="border-0 border-b-2 border-gray-300 p-2 mt-1 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+        class="w-full border-0 border-b-2 border-gray-300 p-2 mt-1 focus:outline-none focus:ring-2 focus:ring-yellow-500"
         placeholder="••••••••"
       />
       <p
-        class="text-red-600 text-sm mt-1"
+        class="text-red-600 text-xs mt-1 h-4"
         :class="{ invisible: !signInPasswordError }"
-        style="min-height: 1.5rem"
       >
         {{ signInPasswordError || '' }}
       </p>
@@ -60,7 +58,10 @@ function handleSignIn(e: Event) {
       >
         Sign In
       </button>
-      <p>{{}}</p>
+      <h4 class="mt-6">
+        Dont't have an account yet?
+        <span class="text-yellow-600 font-semibold cursor-pointer">Sign up</span>
+      </h4>
     </div>
   </form>
 </template>
