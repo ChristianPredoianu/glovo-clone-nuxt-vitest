52c1f7c561d2219a0e62e7f784a1cb1515406735
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-09 14:34:49 +0200

    changed bg color

--- components/ui/LoadingSpinner.vue
+++ components/ui/LoadingSpinner.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center h-screen">
     <div
-      class="w-28 h-28 flex justify-center items-center rounded-full bg-gradient-to-br from-orange-900 via-orange-400 to-orange-900 shadow-lg"
+      class="w-28 h-28 flex justify-center items-center rounded-full bg-green-500 shadow-lg"
     >
       <div
         class="loader w-3/5 h-3/5 border-2 rounded-full border-solid border-white border-opacity-70 border-t-transparent animate-spin"
