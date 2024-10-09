16be24fb03047c27f6a7320aabb7b817f65b5e5a
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-09 14:14:52 +0200

    work

--- components/ui/CloseIcon.vue
+++ components/ui/CloseIcon.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const emits = defineEmits(['closeModal']);
+const emits = defineEmits(['close']);
 </script>
 
 <template>
   <font-awesome-icon
     :icon="['fas', 'close']"
     class="text-2xl text-right mb-8 font-semibold cursor-pointer"
-    @click="emits('closeModal')"
+    @click="emits('close')"
   />
 </template>
