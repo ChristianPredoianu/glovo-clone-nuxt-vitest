376becc279366a0303cdb7364c8d6094a637a735
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-09 14:15:03 +0200

    work

--- components/modals/SignInModal/Overlay/SignInModalOverlay.vue
+++ components/modals/SignInModal/Overlay/SignInModalOverlay.vue
@@ -2,6 +2,8 @@
 import SignInForm from '@/components/forms/SignInForm/SignInForm.vue';
 import SignUpForm from '@/components/forms/SignUpForm/SignUpForm.vue';
 
+const emits = defineEmits(['closeModal']);
+
 const currentComponent = ref<typeof SignInForm | typeof SignUpForm>(markRaw(SignInForm));
 const isSignIn = ref<boolean>(true);
 
@@ -17,11 +19,10 @@ function toggleForm() {
 <template>
   <section class="relative z-40 container mx-auto px-4">
     <div class="flex flex-col gap-7 p-4 md:p-10 w-full">
-      <h3 class="text-center text-3xl font-bold py-10">
+      <h3 class="text-center text-3xl font-bold pb-8">
         {{ currentComponent === SignInForm ? 'Sign In' : 'Sign Up' }}
       </h3>
       <img src="@/sign-in.svg" alt="sign in" class="mx-auto mb-8 w-24 h-24" />
-
       <component :is="currentComponent" />
     </div>
     <button @click="toggleForm">dsadas</button>
