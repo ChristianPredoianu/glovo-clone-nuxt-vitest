f0a8798d8e6b43e6993360a656e31ddf1f10fc20
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-11 12:00:14 +0200

    work

--- composables/auth/useAuth.ts
+++ composables/auth/useAuth.ts
@@ -1,3 +1,5 @@
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 export function useAuth() {
   const {
     validateEmail,
@@ -8,13 +10,26 @@ export function useAuth() {
     repeatedPasswordError,
   } = useAuthValidation();
 
+  const auth = getAuth();
+
   function signUp(email: string, password: string, repeatedPassword: string) {
     const isEmailValid = validateEmail(email);
     const isPasswordValid = validatePassword(password);
     const isRepeatedPasswordValid = validateRepeatedPassword(password, repeatedPassword);
 
     if (isEmailValid && isPasswordValid && isRepeatedPasswordValid) {
-      console.log('Sign up successful');
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
     } else {
       console.log('Sign up failed', repeatedPasswordError.value);
     }
