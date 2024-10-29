8e4761d62306c1105f0ce07ab4eac5f927d1413b
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-29 12:26:20 +0100

    added firebase plugin

---
+++ plugins/firebase.client.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getAnalytics } from 'firebase/analytics';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.public.firebaseApiKey,
+    authDomain: config.public.firebaseAuthDomain,
+    databaseURL: config.public.firebaseDatabaseUrl,
+    projectId: config.public.firebaseProjectId,
+    storageBucket: config.public.firebaseStorageBucket,
+    messagingSenderId: config.public.firebaseMessagingSenderId,
+    appId: config.public.firebaseAppId,
+    measurementId: config.public.firebaseMeasurementId,
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+  const database = getDatabase(app);
+
+  nuxtApp.vueApp.provide('auth', auth);
+  nuxtApp.provide('auth', auth);
+
+  nuxtApp.vueApp.provide('database', database);
+  nuxtApp.provide('database', database);
+});
