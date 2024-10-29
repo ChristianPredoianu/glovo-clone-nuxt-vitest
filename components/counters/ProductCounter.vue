50f39992866d8aa4c6ee2f46cd2581d8b8e52171
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-29 17:40:15 +0100

    added cursor pointer

--- components/counters/ProductCounter.vue
+++ components/counters/ProductCounter.vue
@@ -12,14 +12,14 @@ const { addToCart, removeFromCart } = useCart();
 <template>
   <div class="flex space-between items-center justify-center gap-2">
     <p
-      class="bg-gray-500 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8"
+      class="bg-gray-500 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8 cursor-pointer"
       @click="removeFromCart(product.id)"
     >
       -
     </p>
     <p class="border border-gray-500 py-1 px-3">{{ quantity }}</p>
     <p
-      class="bg-gray-400 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8"
+      class="bg-gray-400 text-gray-100 rounded-sm flex items-center justify-center w-8 h-8 cursor-pointer"
       @click="addToCart(product)"
     >
       +
