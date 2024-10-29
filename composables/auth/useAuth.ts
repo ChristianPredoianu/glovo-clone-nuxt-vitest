c097637de9dfae717db9154eeafdaf4b7b206cff
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-29 13:10:43 +0100

    added user variable ond onMounted to chef for user

--- composables/auth/useAuth.ts
+++ composables/auth/useAuth.ts
@@ -1,18 +1,29 @@
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import type { User } from 'firebase/auth';
 
 export function useAuth() {
+  const authErrorMessage = ref('');
+  const user = ref<User | null>(null); // Type the user as User or null
+
+  const auth = getAuth();
+  const router = useRouter();
+
   const {
     validateEmail,
     validatePassword,
     validateRepeatedPassword,
     emailError,
-
+    passwordError,
     repeatedPasswordError,
   } = useAuthValidation();
 
-  const auth = getAuth();
-
   function signUp(email: string, password: string, repeatedPassword: string) {
+    authErrorMessage.value = '';
     const isEmailValid = validateEmail(email);
     const isPasswordValid = validatePassword(password);
     const isRepeatedPasswordValid = validateRepeatedPassword(password, repeatedPassword);
@@ -20,26 +31,57 @@ export function useAuth() {
     if (isEmailValid && isPasswordValid && isRepeatedPasswordValid) {
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-          // Signed up
           const user = userCredential.user;
           console.log(user);
-          // ...
         })
         .catch((error) => {
           const errorCode = error.code;
-          const errorMessage = error.message;
-          // ..
+          authErrorMessage.value = error.message;
         });
     } else {
-      console.log('Sign up failed', repeatedPasswordError.value);
+      console.log('Sign up failed');
     }
   }
 
-  function signIn(email: string, password: string) {}
+  function signIn(email: string, password: string) {
+    authErrorMessage.value = '';
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid = validatePassword(password);
+
+    if (isEmailValid && isPasswordValid) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          router.push('/dashboard');
+        })
+        .catch((error) => {
+          authErrorMessage.value = error.code;
+          /*   authErrorMessage.value = error.message; */
+        });
+    } else {
+      console.log('Sign in failed', emailError.value, passwordError.value);
+    }
+  }
+
+  onMounted(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        user.value = currentUser;
+        console.log('User is signed in:', user.value);
+      } else {
+        user.value = null;
+        console.log('No user is signed in.');
+      }
+    });
+  });
 
   return {
     emailError,
     repeatedPasswordError,
+    signIn,
     signUp,
+    user,
+    authErrorMessage,
   } as const;
 }
