f396823956e28bae455b9a9cb29cfc31b47784e7
Christian Predoianu <christianpredoianu@yahoo.com>
2024-10-29 12:26:36 +0100

    added env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-console.log(process.env);
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
   imports: {
     dirs: ['composables', 'composables/auth', 'composables/ui'],
@@ -12,15 +12,22 @@ export default defineNuxtConfig({
       apiEdamam: process.env.NUXT_PUBLIC_API_EDAMAM,
       apiRestCountries: process.env.NUXT_PUBLIC_API_COUNTRIES,
       fakeStoreBase: process.env.NUXT_FAKESTORE_BASE,
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseDatabaseUrl: process.env.FIREBASE_DATABASE_URL,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
     },
   },
-  plugins: ['~/plugins/firebase.ts'],
 
   devtools: { enabled: true },
 
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-vitest'],
+
   googleFonts: {
-    // Options
     families: {
       Raleway: {
         wght: [100, 400],
@@ -28,7 +35,9 @@ export default defineNuxtConfig({
       },
     },
   },
+
   css: ['@fortawesome/fontawesome-svg-core/styles.css', '@/assets/transitions.css'],
+
   components: [
     { path: '@/components/header', extensions: ['vue'] },
     { path: '@/components/header/links', extensions: ['vue'] },
