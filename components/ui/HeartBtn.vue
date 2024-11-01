fa2f8c3a7de07fe464ae197483c3cecaf4602a99
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-01 09:57:00 +0100

    added component

---
+++ components/ui/HeartBtn.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const isFavorite = ref<boolean>(false);
+
+function toggleFavorite() {
+  isFavorite.value = !isFavorite.value;
+}
+</script>
+
+<template>
+  <button
+    @click.stop="toggleFavorite"
+    class="cursor-pointer"
+    :class="[
+      isFavorite ? 'text-red-500 border-red-500' : 'text-gray-500 border-gray-300',
+    ]"
+  >
+    <font-awesome-icon :icon="['fas', 'fa-heart']" />
+  </button>
+</template>
