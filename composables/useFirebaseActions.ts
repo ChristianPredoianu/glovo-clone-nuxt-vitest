d23c3e11ebdb4a265a2e509979abf0272311ed72
Christian Predoianu <christianpredoianu@yahoo.com>
2024-11-01 12:00:57 +0100

    work on actions

--- composables/useFirebaseActions.ts
+++ composables/useFirebaseActions.ts
@@ -1,11 +1,14 @@
 import { getDatabase, ref, set, push } from 'firebase/database';
+
 import type { IItem } from '@/interfaces/interfaces.interface';
 
 export function useFirebaseActions() {
+  const { user } = useAuth();
+
   const db = getDatabase();
 
   function writeFavoriteUserItemData(favoriteItem: IItem) {
-    const favoriteItemRef = ref(db, 'favoriteItems');
+    const favoriteItemRef = ref(db, 'users/' + user.value?.uid + '/favoriteItems');
     const newFavoriteItemRef = push(favoriteItemRef);
     return set(newFavoriteItemRef, favoriteItem);
   }
